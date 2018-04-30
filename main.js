2c3d2c52bddfcd21efc04a8e7b2735490a1c8b56
Gabriel Casullo <info@casu-net.com.ar>
2018-04-30 08:24:43 -0300

    adding some style ;-)

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 (function (imgCrypter) {
-  imgCrypter.setCanvas(document.getElementById('preview-canvas'));
+  var canvas = document.getElementById('preview-canvas');
+  var downloadButton = document.getElementById('download-button');
+  var imageInput = document.getElementById('image-input');
 
-  document.getElementById('upload-form').addEventListener('submit', function (event) {
+  imgCrypter.setCanvas(canvas);
+
+  imageInput.addEventListener('change', function (event) {
     event.preventDefault();
-    var imageInput = document.getElementById('image-input');
     if (imageInput && imageInput.files && imageInput.files.length) {
       var reader = new FileReader();
       reader.readAsDataURL(imageInput.files[0]);
       reader.onload = function (e) {
         imgCrypter.loadCanvas(e.target.result);
+        downloadButton.setAttribute('style', 'display: inline');
       }
     }
   });
 
-  document.getElementById('download-button').addEventListener('click', function (event) {
+  downloadButton.addEventListener('click', function (event) {
     event.preventDefault();
-    imgCrypter.download('vamoscarajo2.jpg');
+    imgCrypter.download('result.jpg');
   });
 
   return this;
