f29f59e14295b89870febea1ee41dfca0bf4ba55
Gabriel Casullo <info@casu-net.com.ar>
2018-04-30 09:24:50 -0300

    remove imgCrypter.ready() implementation from custom
    use library methods from handler, since at that point everything should
    be already loaded

--- custom.js
+++ custom.js
@@ -1,14 +1,9 @@
-imgCrypter.ready = function (imageData) {
-  this.setPixel(0, 0, { r: 0, g: 255, b: 0 })
-  for(var i = 10; i <= 200; i++) {
-    for(var j = 0; j <= 50; j++) {
-      this.setPixel(i, j, { r: 255, g: 0, b: 0 });
-    }
-  }
-  this.applyChanges();
-}
-
 function insertMessageAction(message) {
   // Acá va el código que toma el mensaje y lo inserta en la imagen
-  console.log('MESSAGE', message)
+  for(var i = 10; i <= 200; i++) {
+    for(var j = 0; j <= 50; j++) {
+      imgCrypter.setPixel(i, j, { r: 255, g: 0, b: 0 });
+    }
+  }
+  imgCrypter.applyChanges();
 }
